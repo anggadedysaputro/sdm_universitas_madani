ebe7a8254216497f3b7c21ad22a8fac007a78cb2
anggadedysaputro <eanggays@gmail.com>
2024-07-03 05:24:35 +0700

    fix icons

--- resources/views/components/tabler/default/body-open.blade.php
+++ resources/views/components/tabler/default/body-open.blade.php
@@ -15,6 +15,15 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 	<title>@yield('title')</title>
 	<!-- CSS files -->
+    <style>
+		@font-face {
+			font-family: "tabler-icons";
+			font-style: normal;
+			font-weight: 400;
+			src: url("{{ asset('assets/icons/tabler-icons.eot') }}");
+			src: url("{{ asset('assets/icons/tabler-icons.eot') }}") format("embedded-opentype"), url("{{ asset('assets/icons/tabler-icons.woff2') }}") format("woff2"), url("{{ asset('assets/icons/tabler-icons.woff') }}") format("woff"), url("{{ asset('assets/icons/tabler-icons.ttf') }}") format("truetype")
+		}
+	</style>
 	<x-vite/>
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
 </head>
