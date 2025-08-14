fa704a4ce8fb4b1e695bf2b0a5c9e8dd89aef1f8
anggadedysaputro <eanggays@gmail.com>
2025-08-14 17:49:54 +0700

    test

--- app/Http/Controllers/Api/Presensi/ApiPresensi.php
+++ app/Http/Controllers/Api/Presensi/ApiPresensi.php
@@ -174,6 +174,11 @@ class ApiPresensi extends Controller
 
             $jasper = new PHPJasper;
 
+            dd($jasper->process(
+                $input,
+                $output,
+                $options
+            )->output());
 
             $jasper->process(
                 $input,
