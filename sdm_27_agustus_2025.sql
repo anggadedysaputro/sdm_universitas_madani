31a5d19a4d1f78551abda1c4d93c4c0c0139cb40
anggadedysaputro <eanggays@gmail.com>
2025-08-27 11:47:18 +0700

    fix cuti

--- sdm_27_agustus_2025.sql
+++ sdm_27_agustus_2025.sql
@@ -1,3 +1,4 @@
+delete from applications.cuti;
 ALTER TABLE applications.cuti ADD COLUMN nopeg_atasan VARCHAR(20) NOT NULL;
 ALTER TABLE applications.cuti
   ADD CONSTRAINT cuti_fk1 FOREIGN KEY (nopeg_atasan)
