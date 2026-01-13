3c8c6c299cab5ec5dbdcd127f863ae1777c7c24c
nisriinanh <nisriinanh@gmail.com>
2026-01-13 15:47:44 +0700

    fix bootstrap load

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,104 +1,134 @@
 /**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
+ * jQuery (WAJIB PALING ATAS)
  */
 import $ from "jquery";
+window.$ = window.jQuery = $;
+
+
+/**
+ * Axios
+ */
 import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+
+/**
+ * Library umum
+ */
 import Cropper from 'cropperjs';
 window.Cropper = Cropper;
 
 import Angga from './angga';
 import AnggaTables from './anggatables';
+
 import smartWizard from 'smartwizard';
 window.smartWizard = smartWizard();
 import "smartwizard/dist/css/smart_wizard_all.css";
+
 import select2 from 'select2';
 select2();
+
 import 'jstree';
+
 import toastr from 'toastr';
-window.$ = $;
-window.jQuery = $;
-window.Angga = Angga;
 window.toastr = toastr;
-// Angga.initializeDatePickerIndoView();
-window.AnggaTables = AnggaTables;
 
 import Swal from 'sweetalert2'
 window.Swal = Swal;
+
+window.Angga = Angga;
+window.AnggaTables = AnggaTables;
+
+
+/**
+ * =========================
+ * DATATABLES (URUTAN PENTING)
+ * =========================
+ */
+
+// CORE
 import DataTable from 'datatables.net-bs5';
 import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
 window.DataTable = DataTable;
+
+// // EXTENSIONS (SETELAH CORE)
+
+// // Buttons
+// import 'datatables.net-buttons-bs5';
+// import 'datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css';
+// import 'datatables.net-buttons/js/buttons.colVis.mjs';
+
+// // Select (WAJIB UNTUK SEARCHPANES)
+// import 'datatables.net-select-bs5';
+// import 'datatables.net-select-bs5/css/select.bootstrap5.min.css';
+
+// // SearchPanes
+// import 'datatables.net-searchpanes-bs5';
+// import 'datatables.net-searchpanes-bs5/css/searchPanes.bootstrap5.min.css';
+
+// // Responsive
+// import 'datatables.net-responsive-bs5';
+// import 'datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css';
+
+// // RowGroup (opsional)
+// import 'datatables.net-rowgroup-bs5';
+
+
+/**
+ * Flatpickr
+ */
 import "flatpickr/dist/flatpickr.min.css";
 import flatpickr from "flatpickr";
-import 'datatables.net-buttons-bs5';
-import 'datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css';
-import 'datatables.net-rowgroup-bs5';
-import 'datatables.net-searchpanes-bs5';
-import 'datatables.net-select-bs5';
-import 'datatables.net-select-bs5/css/select.bootstrap5.min.css';
-import 'datatables.net-searchpanes-bs5/css/searchPanes.bootstrap5.min.css';
-import 'datatables.net-responsive-bs5';
-import 'datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css';
-import 'datatables.net-buttons/js/buttons.colVis.mjs';
 import monthSelectPlugin from "flatpickr/dist/plugins/monthSelect"
 import "flatpickr/dist/plugins/monthSelect/style.css";
 window.flatpickr = flatpickr;
 window.monthSelectPlugin = monthSelectPlugin;
+
+
+/**
+ * Inputmask
+ */
 import Inputmask from "inputmask";
 window.Inputmask = Inputmask;
-var selectorinteger = document.getElementsByClassName("integer-mask");
-var iminteger = new Inputmask({
-    regex: "^[0-9]*$"
-});
-iminteger.mask(selectorinteger);
 
-var selectordecimal = document.getElementsByClassName("decimal-mask");
-var imdecimal = new Inputmask("decimal", {
-    min: 0,
-    max: 100
-});
-imdecimal.mask(selectordecimal);
+new Inputmask({ regex: "^[0-9]*$" }).mask(".integer-mask");
+
+new Inputmask("decimal", { min: 0, max: 100 }).mask(".decimal-mask");
 
 Inputmask('numeric', {
     radixPoint: ",",
     rightAlign: false,
     digits: 2,
-    onBeforeMask: function (value, opts) {
-        return value;
-    }
 }).mask('.decimal-mask-morethan');
 
 Inputmask({
     mask: "*{1,50}[.*{1,50}][.*{1,50}][.*{1,50}]@*{1,20}[.*{2,6}][.*{1,2}]",
-    greedy: false,
-    onBeforePaste: function (pastedValue, opts) {
-        pastedValue = pastedValue.toLowerCase();
-        return pastedValue.replace("mailto:", "");
-    },
-    definitions: {
-        "*": {
-            validator: '[0-9A-Za-z!#$%&"*+/=?^_`{|}~\-]',
-            cardinality: 1,
-            casing: "lower"
-        }
-    }
+    greedy: false
 }).mask(".email-mask");
 
 Inputmask('numeric', {
     radixPoint: ",",
     allowMinus: false,
-    regex: "[0-9]*",
     groupSeparator: ".",
     rightAlign: false,
-    digits: 2, min: 0,
-    alias: 'numeric',
-    onBeforeMask: function (value, opts) {
-        return value;
-    }
+    digits: 2,
 }).mask('.money-mask');
+
+
+/**
+ * Compressor
+ */
+import Compressor from 'compressorjs';
+window.Compressor = Compressor;
+
+
+/**
+ * CSS tambahan
+ */
+import '../css/datatables.css';
+import '../css/cropper.min.css';
+
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -121,7 +151,3 @@ Inputmask('numeric', {
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
-import Compressor from 'compressorjs';
-window.Compressor = Compressor;
-import '../css/datatables.css';
-import '../css/cropper.min.css';
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,6 @@
         <div class="boni">angga</div>
     </body>
 </html>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script>
    
     $(document).ready(function(){
